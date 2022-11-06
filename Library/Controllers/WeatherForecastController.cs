666a00b5fab0bf6dcea0647fe090c29ed03db462
LilChihba <danila-yurov@mail.ru>
2022-11-06 19:17:16 +0300

    Add API

---
+++ Library/Controllers/WeatherForecastController.cs
@@ -0,0 +1,32 @@
+﻿using Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Library.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class WeatherForecastController : ControllerBase
+    {
+        private ILoggerManager _logger;
+        //private readonly IRepositoryManager _repository;
+        public WeatherForecastController(ILoggerManager logger) //, IRepositoryManager repository)
+        {
+            _logger = logger;
+            //_repository = repository;
+        }
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            _logger.LogInfo("Вот информационное сообщение от нашего контроллера значений.");
+            _logger.LogDebug("Вот отладочное сообщение от нашего контроллера значений.");
+            _logger.LogWarn("Вот сообщение предупреждения от нашего контроллера значений.");
+            _logger.LogError("Вот сообщение об ошибке от нашего контроллера значений.");
+            return new string[] { "value1", "value2" };
+        }
+    }
+}
---
+++ Library/Extensions/ServiceExtensions.cs
@@ -0,0 +1,31 @@
+﻿using Interfaces;
+using LoggerService;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Library.Extensions
+{
+    public static class ServiceExtensions
+    {
+        public static void ConfigureCors(this IServiceCollection services) =>
+        services.AddCors(options =>
+        {
+            options.AddPolicy("CorsPolicy", builder =>
+            builder.AllowAnyOrigin()
+            .AllowAnyMethod()
+            .AllowAnyHeader());
+        });
+
+        public static void ConfigureIISIntegration(this IServiceCollection services) => services.Configure<IISOptions>(options => {});
+
+        public static void ConfigureLoggerService(this IServiceCollection services) =>
+        services.AddScoped<ILoggerManager, LoggerManager>();
+
+        //public static void ConfigureSqlContext(this IServiceCollection services, IConfiguration configuration) => services.AddDbContext<RepositoryContext>(opts => opts.UseSqlServer(configuration.GetConnectionString("sqlConnection"), b => b.MigrationsAssembly("Library")));
+        //public static void ConfigureRepositoryManager(this IServiceCollection services) => services.AddScoped<IRepositoryManager, RepositoryManager>();
+
+        //public static IMvcBuilder AddCustomCSVFormatterCompany(this IMvcBuilder builder) => builder.AddMvcOptions(config => config.OutputFormatters.Add(new CsvOutputFormatterCompany()));
+        //public static IMvcBuilder AddCustomCSVFormatterOrder(this IMvcBuilder builder) => builder.AddMvcOptions(config => config.OutputFormatters.Add(new CsvOutputFormatterOrder()));
+    }
+}

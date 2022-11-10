4c1b74412e5c87b41b8b230f222f2903e76b96f7
LilChihba <danila-yurov@mail.ru>
2022-11-10 19:56:30 +0300

    Fix

---
+++ ReadBook/ResetPassword.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ReadBook
+{
+    /// <summary>
+    /// Логика взаимодействия для ResetPassword.xaml
+    /// </summary>
+    public partial class ResetPassword : Window
+    {
+        public ResetPassword()
+        {
+            InitializeComponent();
+        }
+
+        private void ExitImage_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void RollupImage_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            this.WindowState = WindowState.Minimized;
+        }
+    }
+}
